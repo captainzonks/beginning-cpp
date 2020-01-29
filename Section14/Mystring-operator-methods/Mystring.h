e7dcd60212c7e00c091fac543f81d581d5f6d284
captainzonks <zonks.zonks@gmail.com>
2020-01-28 20:41:29 -0700

    added operator method overloading project

---
+++ Section14/Mystring-operator-methods/Mystring.h
@@ -0,0 +1,29 @@
+#ifndef _MYSTRING_H_
+#define _MYSTRING_H_
+
+class Mystring
+{
+private:
+    char *str;          // pointer to char[] that holds a C-style string
+public:
+    Mystring();         // No-args constructor
+    Mystring(const char *s);    // Overloaded constructor
+    Mystring(const Mystring &source);     // Copy constructor
+    Mystring(Mystring &&source);        // Move constructor
+    ~Mystring();        // Destructor
+    
+    Mystring &operator=(const Mystring &rhs);           // Copy assignment
+    Mystring &operator=(Mystring &&rhs);                    // Move assignment
+    
+    Mystring operator-() const;             // make lowercase
+    Mystring operator+(const Mystring &rhs) const;            // concatenate
+    bool operator==(const Mystring &rhs) const;
+    
+    // getters
+    void display() const;
+    int get_length() const;
+    const char *get_str() const;
+
+};
+
+#endif // _MYSTRING_H_
