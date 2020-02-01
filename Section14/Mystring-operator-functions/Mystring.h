c3fdd00a42c6daa2f888edcc1926a4e2121d3549
captainzonks <zonks.zonks@gmail.com>
2020-02-01 12:52:03 -0700

    continued work in Section 14 overloading operators

---
+++ Section14/Mystring-operator-functions/Mystring.h
@@ -0,0 +1,30 @@
+#ifndef _MYSTRING_H_
+#define _MYSTRING_H_
+
+class Mystring
+{
+    friend bool operator==(const Mystring &lhs, const Mystring &rhs);
+    friend Mystring operator-(const Mystring &obj);
+    friend Mystring operator+(const Mystring &lhs, const Mystring &rhs);
+private:
+    char *str;          // pointer to char[] that holds a C-style string
+public:
+    Mystring();         // No-args constructor
+    Mystring(const char *s);    // Overloaded constructor
+    Mystring(const Mystring &source);     // Copy constructor
+    Mystring(Mystring &&source);        // Move constructor
+    ~Mystring();        // Destructor
+    
+    Mystring &operator=(const Mystring &rhs);           // Copy assignment
+    Mystring &operator=(Mystring &&rhs);                    // Move assignment
+    
+    
+    
+    // getters
+    void display() const;
+    int get_length() const;
+    const char *get_str() const;
+
+};
+
+#endif // _MYSTRING_H_
